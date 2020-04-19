97704f9fb084d129fe7602c7151d63923f72b43e
befi8957 <yamen.ajjour@uni-weimar.de>
2020-04-19 11:53:51 +0200

    adding cluster code

--- generate_args_me.py
+++ generate_args_me.py
@@ -1,5 +1,7 @@
 from conf.configuration import *
 import json
+import logging
+logging.buildConfigu(filename="generate-args-me.log",level=logging.DEBUG)
 import os
 path_source_args_me=get_source_path("args-me")
 print(path_source_args_me)
@@ -7,9 +9,12 @@ root_path="/mnt/ceph/storage/data-in-progress/args-me/2020-04-11/"
 portals=['debateorg','debatepedia','debatewise','idebate']
 all_data=[]
 for portal in portals:
+
     path=root_path+portal+".json"
+    logging.warning("loading up %s"%path)
     with open(path) as json_file:
         data = json.load(json_file)
         all_data.extend(data)
+logging.warning("writing %s"%path_source_args_me)
 with open(path_source_args_me,'w') as outfile:
     json.dump(data,outfile)
