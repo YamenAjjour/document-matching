0c93f58b4d1f6a67b1f912c9bbee175755f81844
yamen.ajjour@uni-weimar.de <yamen.ajjour@uni-weimar.de>
2021-04-21 15:57:09 +0200

    fixing deployment

--- deploy/clean_args_me.sh
+++ deploy/clean_args_me.sh
@@ -1,4 +1,5 @@
-export PYTHONPATH=../src/
-python3 ../src/runs/run_preprocess_args_me.py
-python3 ../src/runs/run_clean_ids.py
-python3 ../src/runs/run_clean_hash.py
+export PYTHONPATH="src/:./"
+echo $PYTHONPATH
+python3 src/runs/run_preprocess_args_me.py
+python3 src/runs/run_clean_ids.py
+python3 src/runs/run_clean_hash.py
